400dc954bb5b08765607cfab028d0de5f349b9e6
Martinius <mar109us@gmail.com>
2026-06-16 19:44:22 +0200

    testing max cookie size and total cookie storage

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+const initStats = document.getElementById("init-stats");
 const createNewCookie = document.getElementById("create-new-cookie");
 const deleteAllCookies = document.getElementById("delete-all-cookies");
 const displayAllCookies = document.getElementById("display-all-cookies");
 let x = document.cookie;
 
-deleteAllCookies.addEventListener("click", clearAllCookies);
+initStats.addEventListener("click", doInitStats);
 createNewCookie.addEventListener("click", createNewRandomCookie);
+deleteAllCookies.addEventListener("click", clearAllCookies);
 
 function setCookie(setCookieKey, setCookieValue, setCookieExpire) {
 	const localDate = new Date();
@@ -49,5 +51,72 @@ viewCookies();
 
 function createNewRandomCookie() {
 	setCookie(`KEY_${Math.random()}`, `VALUE_${Math.random()}`, 999);
+	viewCookies();
+}
+
+let goMaxCookieSize = "0";
+
+function doInitStats() {
+	clearAllCookies();
+
+	// max kB = 15 961
+
+	//max per cookie
+	/* 	setCookie(
+		goMaxCookieSize.repeat(2048),
+		goMaxCookieSize.repeat(2048),
+		3650000,
+	); */
+	setCookie(
+		`${goMaxCookieSize.repeat(1000)}`,
+		goMaxCookieSize.repeat(1000),
+		3650000,
+	);
+	setCookie(
+		`1${goMaxCookieSize.repeat(999)}`,
+		goMaxCookieSize.repeat(1000),
+		3650000,
+	);
+	setCookie(
+		`11${goMaxCookieSize.repeat(998)}`,
+		goMaxCookieSize.repeat(1000),
+		3650000,
+	);
+	setCookie(
+		`111${goMaxCookieSize.repeat(997)}`,
+		goMaxCookieSize.repeat(1000),
+		3650000,
+	);
+	setCookie(
+		`1111${goMaxCookieSize.repeat(996)}`,
+		goMaxCookieSize.repeat(1000),
+		3650000,
+	);
+	setCookie(
+		`11111${goMaxCookieSize.repeat(995)}`,
+		goMaxCookieSize.repeat(1000),
+		3650000,
+	);
+	setCookie(
+		`111111${goMaxCookieSize.repeat(994)}`,
+		goMaxCookieSize.repeat(1000),
+		3650000,
+	);
+	setCookie(
+		`1111111${goMaxCookieSize.repeat(493)}`,
+		goMaxCookieSize.repeat(500),
+		3650000,
+	);
+	setCookie(
+		`11111111${goMaxCookieSize.repeat(100)}`,
+		goMaxCookieSize.repeat(100),
+		3650000,
+	);
+	setCookie(
+		`${goMaxCookieSize.repeat(200)}`,
+		goMaxCookieSize.repeat(283),
+		3650000,
+	);
+
 	viewCookies();
 }
