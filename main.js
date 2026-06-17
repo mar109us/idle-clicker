9880c42c8139a1d8288eb6d1744a20773534e2df
Martinius <mar109us@gmail.com>
2026-06-17 12:38:53 +0200

    using cookies are very limiting for save state as some browsers defualt to 7 day expire while others have max 700 day expire.
    continue for fun

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
 const initStats = document.getElementById("init-stats");
-const createNewCookie = document.getElementById("create-new-cookie");
 const deleteAllCookies = document.getElementById("delete-all-cookies");
 const displayAllCookies = document.getElementById("display-all-cookies");
-let x = document.cookie;
 
 initStats.addEventListener("click", doInitStats);
-createNewCookie.addEventListener("click", createNewRandomCookie);
 deleteAllCookies.addEventListener("click", clearAllCookies);
 
 function setCookie(setCookieKey, setCookieValue, setCookieExpire) {
-	const localDate = new Date();
-	localDate.setTime(
-		localDate.getTime() + setCookieExpire * 24 * 60 * 60 * 1000,
-	);
-	let expires = "expires=" + localDate.toUTCString();
 	document.cookie =
-		setCookieKey + "=" + setCookieValue + ";" + expires + ";path=/";
+		setCookieKey + "=" + setCookieValue + "; Max-Age=2000000000; Path=/";
 }
 
 function getAllCookieNames() {
@@ -49,76 +41,34 @@ function viewCookies() {
 }
 viewCookies();
 
-function createNewRandomCookie() {
-	setCookie(`KEY_${Math.random()}`, `VALUE_${Math.random()}`, 999);
-	viewCookies();
-}
-
-let goMaxCookieSize = "0";
-
 function doInitStats() {
 	clearAllCookies();
 
 	// max = 15 691
 	// max cookie = 4096
 	// max cookies(safari) = 50
+	// cookie deletion is restricting functionality very much
+	// apple default to 7 day deletion
+	// brave default to 6 months deletion
 
-	//max per cookie
-	/* 	setCookie(
-		goMaxCookieSize.repeat(2048),
-		goMaxCookieSize.repeat(2048),
-		3650000,
-	); */
-	/* 	setCookie(
-		`${goMaxCookieSize.repeat(1000)}`,
-		goMaxCookieSize.repeat(1000),
-		3650000,
-	);
-	setCookie(
-		`1${goMaxCookieSize.repeat(999)}`,
-		goMaxCookieSize.repeat(1000),
-		3650000,
-	);
-	setCookie(
-		`11${goMaxCookieSize.repeat(998)}`,
-		goMaxCookieSize.repeat(1000),
-		3650000,
-	);
-	setCookie(
-		`111${goMaxCookieSize.repeat(997)}`,
-		goMaxCookieSize.repeat(1000),
-		3650000,
-	);
-	setCookie(
-		`1111${goMaxCookieSize.repeat(996)}`,
-		goMaxCookieSize.repeat(1000),
-		3650000,
-	);
-	setCookie(
-		`11111${goMaxCookieSize.repeat(995)}`,
-		goMaxCookieSize.repeat(1000),
-		3650000,
-	);
-	setCookie(
-		`111111${goMaxCookieSize.repeat(994)}`,
-		goMaxCookieSize.repeat(1000),
-		3650000,
-	);
-	setCookie(
-		`1111111${goMaxCookieSize.repeat(493)}`,
-		goMaxCookieSize.repeat(500),
-		3650000,
-	);
-	setCookie(
-		`11111111${goMaxCookieSize.repeat(100)}`,
-		goMaxCookieSize.repeat(100),
-		3650000,
-	);
-	setCookie(
-		`${goMaxCookieSize.repeat(200)}`,
-		goMaxCookieSize.repeat(283),
-		3650000,
-	); */
+	setCookie(user.character, "", 1);
 
 	viewCookies();
 }
+const user = {
+	character: {
+		xp: 100,
+		health: 100,
+		stamina: 100,
+	},
+};
+
+function splitObjects() {}
+
+let test = `${Object.entries(user.character)}`
+
+
+console.log(test);
+
+console.log(Object.keys(user.character));
+console.log(Object.values(user.character));
