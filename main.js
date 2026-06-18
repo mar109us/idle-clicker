538d86c9ecf30d50bed88762de21411468e77ac3
Martinius <mar109us@gmail.com>
2026-06-18 22:23:03 +0200

    testing out different ways of storing data in cookies

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const initStats = document.getElementById("init-stats");
 const deleteAllCookies = document.getElementById("delete-all-cookies");
+const checkCookie = document.getElementById("check-cookie");
+
 const displayAllCookies = document.getElementById("display-all-cookies");
 
 initStats.addEventListener("click", doInitStats);
 deleteAllCookies.addEventListener("click", clearAllCookies);
+checkCookie.addEventListener("click", isCookieEmpty);
 
 function setCookie(setCookieKey, setCookieValue, setCookieExpire) {
 	document.cookie =
@@ -21,7 +24,7 @@ function clearAllCookies() {
 	cookies.forEach((cookieName) => {
 		document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
 	});
-	console.log("Cleared all cookies");
+	console.log("Cookie: Cleared");
 	viewCookies();
 }
 
@@ -35,24 +38,30 @@ function modifyCookieOutput() {
 }
 
 const user = {
+	creation: null,
 	character: {
-		xp: 100,
+		experience: 100,
 		health: 100,
 		stamina: 100,
 	},
 };
 
-function doInitStats() {
+function setUserCreationTime() {
+	const newTime = new Date();
+	user.creation = newTime.getTime();
+}
+
+function isCookieEmpty() {
+	console.log("Cookie: Check");
+	if (document.cookie) {
+		console.log("Cookie: Exist");
+	} else (console.log("Cookie: Not found"), doInitStats());
+}
+
+/* function doInitStats() {
 	clearAllCookies();
 
-	// max = 15 691
-	// max cookie = 4096
-	// max cookies(safari) = 50
-	// cookie deletion is restricting functionality very much
-	// apple default to 7 day deletion
-	// brave default to 6 months deletion
-	// chrome 700 day deletion
-
+	console.log("Cookie: Set initial values");
 	Object.entries(user.character).forEach((element) => {
 		let elementToString = `${element};`;
 
@@ -62,19 +71,51 @@ function doInitStats() {
 		);
 	});
 
+	console.log("Cookie: Set creation time");
+	setUserCreationTime();
+
+	viewCookies();
+} */
+
+function doInitStats() {
+	clearAllCookies();
+
+	console.log("Cookie: Set initial values");
+
+	let characterKeys = "character=";
+	Object.entries(user.character).forEach((entry) => {
+		characterKeys += `[${entry}]`;
+	});
+	document.cookie = characterKeys;
+
+	console.log("Cookie: Set creation time");
+	setUserCreationTime();
+
 	viewCookies();
 }
 
 function viewCookies() {
-	console.log("Current cookies:", getAllCookieNames());
+	console.log("Cookie:", getAllCookieNames());
 	displayAllCookies.innerHTML = modifyCookieOutput();
-	console.log(document.cookie);
 }
 viewCookies();
 
-user.character.health + 5;
-doInitStats();
+/* user.character.health + 5;
+doInitStats(); */
 
+/* setInterval(update, 1000)
+
+function update() {
+	console.log("mew")
+} */
+
+// max = 15 691
+// max cookie = 4096
+// max cookies(safari) = 50
+// cookie deletion is restricting functionality very much
+// apple default to 7 day deletion
+// brave default to 6 months deletion
+// chrome 700 day deletion
 
 // i need to check if user has a cookie, if not, make one.
 // i need to read data from cookie to modify
